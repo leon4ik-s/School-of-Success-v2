f9fcfa77886f09477b8fa21b77ffc319fb0bb596
leon4ik-s <145491660+leon4ik-s@users.noreply.github.com>
2025-10-29 22:26:25 +0300

    Update iq.js

--- js/iq.js
+++ js/iq.js
@@ -1,8 +1,12 @@
-const button = document.getElementById('iq-btn');
-const result = document.getElementById('iq-result');
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('iq-btn');
+  const result = document.getElementById('iq-result');
 
-button.addEventListener('click', () => {
-  const iq = Math.floor(Math.random() * 80) + 80; // 80–160
-  result.innerHTML = `<h2>Ваш IQ: ${iq}</h2>
-                      <p>${iq > 120 ? 'Вы гений!' : 'Продолжайте развиваться!'}</p>`;
+  button.addEventListener('click', () => {
+    const iq = Math.floor(Math.random() * 80) + 80; // 80–160
+    result.innerHTML = `
+      <h2>Ваш IQ: ${iq}</h2>
+      <p>${iq > 120 ? 'Вы гений!' : 'Продолжайте развиваться!'}</p>
+    `;
+  });
 });
