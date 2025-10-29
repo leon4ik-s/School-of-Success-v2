d19751d101d6d909218390cc965ac9e023122e04
leon4ik-s <145491660+leon4ik-s@users.noreply.github.com>
2025-10-29 22:06:52 +0300

    Create iq.js

---
+++ ff/js/iq.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('iq-btn');
+const result = document.getElementById('iq-result');
+
+button.addEventListener('click', () => {
+  const iq = Math.floor(Math.random() * 80) + 80; // 80–160
+  result.innerHTML = `<h2>Ваш IQ: ${iq}</h2>
+                      <p>${iq > 120 ? 'Вы гений!' : 'Продолжайте развиваться!'}</p>`;
+});
